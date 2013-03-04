2cdb3925a7537ca67bae963fcc2ee550944e27b6
Ed Summers <ehs@pobox.com>
2013-03-04 03:38:26 -0500

    added brief example of output

--- example.js
+++ example.js
@@ -1,8 +1,25 @@
-/**
- * this example will log into the irc servers with the nickname
- * wikidatachangestest and will print out the page title that changed
- * the user that did the change, and a url for the page.
- */
+/*
+ 
+This example will log into the irc server with the nickname
+wikidatachangestest and monitor only the changes for wikidata. 
+For each change it will print out the page title that changed
+the user that did the change, and a url for the page. For example:
+
+% node example.js
+Q192232 [אבגד] http://Wikidata.org/wiki/Q192232
+Q888455 [BeneBot*] http://Wikidata.org/wiki/Q888455
+Q6117009 [Sk!dbot] http://Wikidata.org/wiki/Q6117009
+Q6117010 [KLBot2] http://Wikidata.org/wiki/Q6117010
+Q888455 [BeneBot*] http://Wikidata.org/wiki/Q888455
+Q6117012 [KLBot2] http://Wikidata.org/wiki/Q6117012
+Q991546 [BeneBot*] http://Wikidata.org/wiki/Q991546
+Q6117013 [Sk!dbot] http://Wikidata.org/wiki/Q6117013
+Q991546 [BeneBot*] http://Wikidata.org/wiki/Q991546
+Q6117014 [KLBot2] http://Wikidata.org/wiki/Q6117014
+Q1079021 [BeneBot*] http://Wikidata.org/wiki/Q1079021
+Q6117015 [Sk!dbot] http://Wikidata.org/wiki/Q6117015
+
+*/
 
 var wikichanges = require("./wikichanges");
 
