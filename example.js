a48743e2ad93d3b47c86c34d4a991c0af53bf965
Ed Summers <ehs@pobox.com>
2013-03-04 03:35:27 -0500

    added a comment to explain what is going on

--- example.js
+++ example.js
@@ -1,3 +1,9 @@
+/**
+ * this example will log into the irc servers with the nickname
+ * wikidatachangestest and will print out the page title that changed
+ * the user that did the change, and a url for the page.
+ */
+
 var wikichanges = require("./wikichanges");
 
 w = new wikichanges.WikiChanges({
